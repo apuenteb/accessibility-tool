1e9764b4b543759fbc8fcb4812eb1a39285992eb
apuenteb <apuentebalda@gmail.com>
2024-12-02 10:44:41 -0500

    major improvements (hopefully)

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import dash
 from dash import html
-import geopandas as gpd
-import json
-
-# Load GeoJSON data
-sections = gpd.read_file('aggregated_buildings_multipolygons_sections.geojson')
-geojson_data = json.loads(sections.to_json())
-
-# Save the Leaflet map HTML with embedded GeoJSON data
-with open("leaflet_map.html", "r") as file:
-    leaflet_template = file.read()
-
-leaflet_map = leaflet_template.replace("{{ geojson_data }}", json.dumps(geojson_data))
-
-with open("leaflet_map_generated.html", "w") as file:
-    file.write(leaflet_map)
+from flask import send_file
 
 # Dash app
 app = dash.Dash(__name__)
 
+# Serve the HTML template directly
+@app.server.route('/leaflet_map')
+def serve_leaflet_map():
+    return send_file('leaflet_map.html')
+
+# Serve the GeoJSON file
+@app.server.route('/geojson')
+def serve_geojson():
+    return send_file('aggregated_buildings_multipolygons_sections.geojson')
+
+# Dash layout
 app.layout = html.Div(
     children=[
         html.Iframe(
-            srcDoc=open("leaflet_map_generated.html", "r").read(),
+            src="/leaflet_map",
             style={"height": "100vh", "width": "100vw", "border": "none"},
         )
     ]
@@ -30,4 +27,5 @@ app.layout = html.Div(
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
