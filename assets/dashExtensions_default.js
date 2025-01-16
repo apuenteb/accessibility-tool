83f6222e8cf55ae2d10a107d492cd7fb901caedc
apuenteb <apuentebalda@gmail.com>
2025-01-16 11:08:11 -0500

    commit prior to change

--- assets/dashExtensions_default.js
+++ assets/dashExtensions_default.js
@@ -1,12 +1,11 @@
 window.dashExtensions = Object.assign({}, window.dashExtensions, {
     default: {
         function0: function(feature) {
-                const choice = "{choice}";
                 return {
                     color: '#3182bd',
                     weight: 2,
                     opacity: 0.8,
-                    fillColor: feature.properties["color"] || '#6baed6',
+                    fillColor: feature.properties.color || '#6baed6',
                     fillOpacity: 0.4
                 };
             }
